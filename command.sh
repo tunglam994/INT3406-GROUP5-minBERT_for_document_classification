43acdda9a683e75fdaabe495615535ffee1ddf19
Ha Tang <121566444+hachane@users.noreply.github.com>
2024-11-12 17:06:23 +0700

    add supervised_CL_bert.pt

--- command.sh
+++ command.sh
@@ -1,6 +1,8 @@
 mkdir -p ANDREWID
 
-python3 classifier.py --option pretrain --epochs 10 --lr 1e-3 --train data/sst-train.txt --dev data/sst-dev.txt --test data/sst-test.txt
+python classifier.py --option pretrain --pretrained_model models/supervised_CL_bert.pt --epochs 5 --batch_size 32 --lr 0.0001 --train data/sst-train.txt --dev data/sst-dev.txt --test data/sst-test.txt
+
+python classifier.py --option pretrain --epochs 5 --batch_size 32 --lr 0.0001 --train data/sst-train.txt --dev data/sst-dev.txt --test data/sst-test.txt
 
 python classifier_unsupervised_CL.py --train data_small/sample_wiki1m.txt --dev data_small/sst-dev.txt --test data_small/sst-test.txt --epochs 5 --batch_size 32 --lr 0.0001 --option finetune
 
