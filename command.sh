57ed1ab7806675ffbfcabd474a7422c67a402d74
tunglam994 <ntl09092004@gmail.com>
2024-11-23 14:01:14 +0700

    modify mlp hidden layers

--- command.sh
+++ command.sh
@@ -3,7 +3,7 @@ mkdir -p ANDREWID
 python3 classifier.py --option pretrain --epochs 10 --lr 1e-3 --train data/sst-train.txt --dev data/sst-dev.txt --test data/sst-test.txt
 
 #use cpu
-python classifier.py --train data/book_summaries_train.csv --dev data/book_summaries_valid.csv --test data/book_summaries_test.csv --dev_out book_summaries_valid_output.txt --test_out book_summaries_test_output.txt --epochs 5 --batch_size 32 --lr 0.0001 --option finetune
+python classifier.py --train data/book_summaries_train.csv --dev data/book_summaries_valid.csv --test data/book_summaries_test.csv --dev_out book_summaries_valid_output.txt --test_out book_summaries_test_output.txt --epochs 5 --batch_size 32 --lr 0.001 --option finetune
 
 #use gpu
 python classifier.py --train data/book_summaries_train.csv --dev data/book_summaries_valid.csv --test data/book_summaries_test.csv --use_gpu --dev_out book_summaries_valid_output.txt --test_out book_summaries_test_output.txt --epochs 5 --batch_size 32 --lr 0.0001 --option finetune
